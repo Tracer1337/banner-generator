b2fd9525b54f664568cb8fbfc599c66e7c1eb492
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-25 18:45:05 +0200

    Create new build

---
+++ public/precache-manifest.03278c0ee646b2550cb0e675f72d3da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c8ca6cf6786002e149e956f1c298f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee70a16214aa7d62e81",
+    "url": "/static/css/main.3e82d80a.chunk.css"
+  },
+  {
+    "revision": "3190a0be19086ee10dce",
+    "url": "/static/js/2.4412f0ef.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.4412f0ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee70a16214aa7d62e81",
+    "url": "/static/js/main.9f576ef7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
