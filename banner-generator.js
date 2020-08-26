9fa2709c89a652d00f7a7003439389a4f4fa68c2
mortis.pro <67234292+mortisdotpro@users.noreply.github.com>
2020-08-26 16:56:48 +0200

    sinusbot backend

---
+++ banner-generator.js
@@ -0,0 +1,8 @@
+registerPlugin({
+    name: 'banner generator',
+    version: '1.0',
+    description: 'does a banner for your teamspeak with time etc',
+    author: 'Mortis <notmortis@notamail.com>',
+    vars: [],
+    autorun: true
+});
