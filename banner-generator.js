7b3505dc2e7cac44b8d5b59861f81a8a0cd3bd92
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-26 17:00:53 +0200

    Create folder for sinusbot script

--- banner-generator.js
+++
@@ -1,8 +0,0 @@
-registerPlugin({
-    name: 'banner generator',
-    version: '1.0',
-    description: 'does a banner for your teamspeak with time etc',
-    author: 'Mortis <notmortis@notamail.com>',
-    vars: [],
-    autorun: true
-});
---
+++ sinusbot-script/main.js
@@ -0,0 +1,8 @@
+registerPlugin({
+    name: 'banner generator',
+    version: '1.0',
+    description: 'Creates a banner for your teamspeak with time etc',
+    author: 'Mortis <notmortis@notamail.com>, Merlin Moelter <merlin.moelter@gmail.com>',
+    vars: [],
+    autorun: true
+});
